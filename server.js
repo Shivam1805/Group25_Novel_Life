02b1d8ce964aa0098319b8bd8d7bae93b3a24370
vidhyaboghara <60362264+vidhyaboghara@users.noreply.github.com>
2020-07-25 20:55:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const eduRoute = require('./src/backend/Route/eduRoute');
 app.use('/api/edu', eduRoute);
 
 app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname + "/public/index.html"))
+    res.sendFile(path.join(__dirname + "/build/index.html"))
 })
 
 //start the app by listening on default port
