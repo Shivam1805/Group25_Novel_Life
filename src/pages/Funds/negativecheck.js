d2285c2675f289dcd894cc9c2ed95ff1bb24be86
vidhyaboghara <vidhya.boghara111@gmail.com>
2020-08-03 14:00:23 -0300

    modifications in funding

--- src/pages/Funds/negativecheck.js
+++ src/pages/Funds/negativecheck.js
@@ -1,22 +1,47 @@
-import React from 'react';
+import React from "react";
 
-import './negativecheck.css';
-import { Row } from 'antd';
+import "./negativecheck.css";
+import { Row } from "antd";
 const negativecheck = () => {
-return(
-
-    <Row style={{justifyContent:"center", height:"100%", width:"100%"}}>
-
-        <div className = "checkwrap3" style={{justifyContent:"center", height:"100%", width:"100%", textAlign:"center"}}>
-            <p style ={{fontSize:"35px", justifyContent:"center", height:"100%", width:"100%", textAlign:"center"}}>
-                <b style={{justifyContent:"center", align:"center", color:"#FFFF", textAlign:"center"}}>
-                    You are NOT eligible for any of the financial benefits</b></p> 
-
-        </div>
-
+  return (
+    <Row style={{ justifyContent: "center", height: "100%", width: "100%" }}>
+      <div
+        className="checkwrap3"
+        style={{
+          justifyContent: "center",
+          height: "100%",
+          width: "100%",
+          textAlign: "center",
+        }}
+      >
+        <p
+          style={{
+            fontSize: "35px",
+            justifyContent: "center",
+            height: "100%",
+            width: "100%",
+            textAlign: "center",
+          }}
+        >
+          <b
+            style={{
+              justifyContent: "center",
+              align: "center",
+              color: "#FFFF",
+              textAlign: "center",
+            }}
+          >
+            You are eligible for CERB
+          </b>
+        </p>
+        <a
+          style={{ fontSize: "18px", textDecoration: "underline" }}
+          href="https://www.canada.ca/en/services/benefits/covid19-emergency-benefits.html"
+        >
+          Know more
+        </a>
+      </div>
     </Row>
-
-)
-
+  );
 };
 export default negativecheck;
--- src/pages/Funds/positivecheck.js
+++ src/pages/Funds/positivecheck.js
@@ -1,21 +1,47 @@
-import React from 'react';
-import './positivecheck.css';
-import {Row} from 'antd';
+import React from "react";
+import "./positivecheck.css";
+import { Row } from "antd";
 
 const positivecheck = () => {
-return(
-
-<Row style={{justifyContent:"center", height:"100%", width:"100%"}}>
-
-<div className = "checkwrap3" style={{justifyContent:"center", height:"100%", width:"100%", textAlign:"center"}}>
-    <p style ={{fontSize:"35px", justifyContent:"center", height:"100%", width:"100%", textAlign:"center"}}>
-        <b style={{justifyContent:"center", align:"center", color:"#FFFF", textAlign:"center"}}>
-            You are eligible for CERB and CEWS</b></p> 
-
-</div>
-
-</Row>
-)
-
+  return (
+    <Row style={{ justifyContent: "center", height: "100%", width: "100%" }}>
+      <div
+        className="checkwrap3"
+        style={{
+          justifyContent: "center",
+          height: "100%",
+          width: "100%",
+          textAlign: "center",
+        }}
+      >
+        <p
+          style={{
+            fontSize: "35px",
+            justifyContent: "center",
+            height: "100%",
+            width: "100%",
+            textAlign: "center",
+          }}
+        >
+          <b
+            style={{
+              justifyContent: "center",
+              align: "center",
+              color: "#FFFF",
+              textAlign: "center",
+            }}
+          >
+            You are eligible for CESB and CEWS
+          </b>
+        </p>
+        <a
+          style={{ fontSize: "18px", textDecoration: "underline" }}
+          href="https://www.canada.ca/en/services/benefits/covid19-emergency-benefits.html"
+        >
+          Know more
+        </a>
+      </div>
+    </Row>
+  );
 };
 export default positivecheck;
